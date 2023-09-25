7f5abd674d0efe46f51eac607e80360b5c40523a
carloscatalinacastello <carlos.catalina.castello@cieep.com>
2023-09-25 17:56:47 +0200

    Añadido Ejercicio2 al Ejercicio1

--- Ejempli1-HolaMundo/ViewController.swift
+++ Ejempli1-HolaMundo/ViewController.swift
@@ -11,15 +11,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var lbSaludo: UILabel!
     
-    
+    @IBOutlet weak var lbCuenta: UILabel!
+    var contador : UInt!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        contador = 0
     }
 
     @IBAction func escribeHolaMundo(_ sender: Any) {
         lbSaludo.text = "Hola mundo"
     }
     
+    
+    @IBAction func contador(_ sender: Any) {
+        contador += 1
+        lbCuenta.text = (String (contador)+" veces")
+    }
 }
 
